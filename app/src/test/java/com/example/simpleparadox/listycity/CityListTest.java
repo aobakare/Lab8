05a54a9bcbafe4e96d6ef2300fe3b1f3ae6cd66a
aobakare <aobakare@ualberta.ca>
2020-10-27 18:32:49 -0600

    passing test

--- app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
+++ app/src/test/java/com/example/simpleparadox/listycity/CityListTest.java
@@ -52,8 +52,8 @@ class CityListTest {
     void testGetCities() {
         CityList cityList = mockCityList();
 
-       // assertEquals(0, mockCity().compareTo(cityList.getCities().get(0))); passing test
-        assertEquals(1, mockCity().compareTo(cityList.getCities().get(0))); //failing test 
+        assertEquals(0, mockCity().compareTo(cityList.getCities().get(0))); //passing test
+        //assertEquals(1, mockCity().compareTo(cityList.getCities().get(0))); //failing test
 
         City city = new City("Charlottetown", "Prince Edward Island");
         cityList.add(city);
